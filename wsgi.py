e8dd74eb3421837ad499e3b1869555f76dfca921
VEADER1005 <sj.sahiljain1005@gmail.com>
2024-01-29 14:02:00 +0530

    changes to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
 from app import create_app
 from waitress import serve
+import socket
 
 app = create_app()
 
+# Get the outbound IP address assigned by Render
+outbound_ip = socket.gethostbyname(socket.gethostname())
+
 if __name__ == "__main__":
-    serve(app, host="localhost", port=5000)
+    # Use the outbound IP address and the port specified by Render
+    serve(app, host=outbound_ip, port=5000)
