76fa0b250caae05f30392bc3723668e4acd7f510
VEADER1005 <sj.sahiljain1005@gmail.com>
2024-01-30 12:00:23 +0530

    changes reverted to wsgi

--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,8 @@ import socket
 app = create_app()
 
 # Get the outbound IP address assigned by Render
-outbound_ip = socket.gethostbyname(socket.gethostname())
+# outbound_ip = socket.gethostbyname(socket.gethostname())
 
 if __name__ == "__main__":
     # Use the outbound IP address and the port specified by Render
-    serve(app, host=outbound_ip, port=5000)
+    serve(app, host="localhost", port=5000)
